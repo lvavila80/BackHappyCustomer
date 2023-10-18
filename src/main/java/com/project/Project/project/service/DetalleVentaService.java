6b1c4f311649774de9fa25761f9efd75dd1148de
LauraRiano143 <lmriano41@ucatolica.edu.co>
2023-10-18 00:57:41 -0500

    Creación detalle venta

---
+++ src/main/java/com/project/Project/project/service/DetalleVentaService.java
@@ -0,0 +1,39 @@
+package com.project.Project.project.service;
+
+import com.project.Project.project.model.ArticuloVentaDTO;
+import com.project.Project.project.model.Venta;
+import com.project.Project.project.model.DetalleVenta;
+import com.project.Project.project.repository.DetalleVentaRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DetalleVentaService {
+
+    @Autowired
+    private DetalleVentaRepository detalleVentaRepository;
+
+    @Autowired
+    private ArticuloService articuloService;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public DetalleVenta guardarDetalleVenta(ArticuloVentaDTO ventaArticulosDTO, Venta savedVenta, int idArticulo) {
+        try {
+            DetalleVenta detalleVenta = new DetalleVenta();
+            detalleVenta.setIdVenta(savedVenta.getId());
+            //detalleVenta.setIdCategoria(ventaArticulosDTO.getIdCategoria());
+            detalleVenta.setValorUnidad(ventaArticulosDTO.getValorUnidad());
+            //detalleVenta.setUnidadesVendidas(ventaArticulosDTO.getUnidadesVendidas());
+            //detalleVenta.setIdArticulo(idArticulo);
+            return detalleVentaRepository.save(detalleVenta);
+        } catch (Exception e) {
+            throw new RuntimeException("Error al guardar venta y relaciones: " + e.getMessage(), e);
+        }
+    }
+}
